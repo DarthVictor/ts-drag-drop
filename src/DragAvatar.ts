8f0ee98a16df2ca3836f9b5f8dee931e614765d7
Victor <follet.victor@gmail.com>
2015-07-01 19:21:37 +0300

    start DragAvatar.ts

--- src/DragAvatar.ts
+++ src/DragAvatar.ts
@@ -12,7 +12,7 @@
  */
 declare function getElementUnderClientXY(elem, clientX, clientY) : HTMLElement
 
-function DragAvatar(dragZone, dragElem) {
+function DragAvatarOld(dragZone, dragElem) {
     /** "родительская" зона переноса */
     this._dragZone = dragZone;
 
@@ -37,7 +37,7 @@ function DragAvatar(dragZone, dragElem) {
  * @param downY Координата Y нажатия мыши
  * @param event Текущее событие мыши
  */
-DragAvatar.prototype.initFromEvent = function(downX, downY, event) {
+DragAvatarOld.prototype.initFromEvent = function(downX, downY, event) {
     /* override */
 };
 
@@ -45,7 +45,7 @@ DragAvatar.prototype.initFromEvent = function(downX, downY, event) {
  * Возвращает информацию о переносимом элементе для DropTarget
  * @param event
  */
-DragAvatar.prototype.getDragInfo = function(event) {
+DragAvatarOld.prototype.getDragInfo = function(event) {
     // тут может быть еще какая-то информация, необходимая для обработки конца или процесса переноса
     return {
         elem: this._elem,
@@ -58,7 +58,7 @@ DragAvatar.prototype.getDragInfo = function(event) {
  * Возвращает текущий самый глубокий DOM-элемент под this._elem
  * Приватное свойство _currentTargetElem обновляется при каждом передвижении
  */
-DragAvatar.prototype.getTargetElem = function() {
+DragAvatarOld.prototype.getTargetElem = function() {
     return this._currentTargetElem;
 };
 
@@ -67,10 +67,9 @@ DragAvatar.prototype.getTargetElem = function() {
  * и записывает текущий элемент под this._elem в _currentTargetElem
  * @param event
  */
-DragAvatar.prototype.onDragMove = function(event) {
+DragAvatarOld.prototype.onDragMove = function(event) {
     this._elem.style.left = event.pageX - this._shiftX + 'px';
     this._elem.style.top = event.pageY - this._shiftY + 'px';
-
     this._currentTargetElem = getElementUnderClientXY(this._elem, event.clientX, event.clientY);
 };
 
@@ -78,13 +77,115 @@ DragAvatar.prototype.onDragMove = function(event) {
  * Действия с аватаром, когда перенос не удался
  * Например, можно вернуть элемент обратно или уничтожить
  */
-DragAvatar.prototype.onDragCancel = function() {
+DragAvatarOld.prototype.onDragCancel = function() {
     /* override */
 };
 
 /**
  * Действия с аватаром после успешного переноса
  */
-DragAvatar.prototype.onDragEnd = function() {
+DragAvatarOld.prototype.onDragEnd = function() {
     /* override */
 };
+
+/**
+ * "Аватар" - элемент, который перетаскивается.
+ *
+ * В простейшем случае аватаром является сам переносимый элемент
+ * Также аватар может быть клонированным элементом
+ * Также аватар может быть иконкой и вообще чем угодно.
+ */
+class DragAvatar {
+  /** "родительская" зона переноса */
+  protected _dragZone: any;
+
+  /**
+   * подэлемент родительской зоны, к которому относится аватар
+   * по умолчанию - элемент, соответствующий всей зоне
+   * может быть уточнен в initFromEvent
+   */
+  protected _dragZoneElem: any;
+
+  /**
+   * Сам элемент аватара, который будет носиться по экрану.
+   * Инициализуется в initFromEvent
+   */
+  protected _elem: any;
+
+  /**
+   * Текущий элемент под this._elem, обновление проиходит в методе onDragMove
+   */
+  protected _currentTargetElem: any;
+
+  /**
+   * вспомогательные свойства, которые хранят координаты точки первого нажатия при начали переноса,
+   * относительно вернего левого угла элемента
+   */
+  protected _shiftX: number;
+  protected _shiftY: number;
+
+  constructor(dragZone: any, dragElem: any) {
+    this._dragZone = dragZone;
+    this._dragZoneElem = dragElem;
+    this._elem = dragElem;
+  }
+
+  /**
+   * Инициализовать this._elem и позиционировать его
+   * При необходимости уточнить this._dragZoneElem
+   * @param downX Координата X нажатия мыши
+   * @param downY Координата Y нажатия мыши
+   * @param event Текущее событие мыши
+   */
+  initFromEvent(downX: number, downY: number, event: Event) {
+    /* override */
+  }
+
+  /**
+   * Возвращает информацию о переносимом элементе для DropTarget
+   * @param event
+   */
+  getDragInfo = function(event: Event) {
+    // тут может быть еще какая-то информация, необходимая для обработки конца или процесса переноса
+    return {
+      elem: this._elem,
+      dragZoneElem: this._dragZoneElem,
+      dragZone: this._dragZone
+    };
+  }
+
+  /**
+   * Возвращает текущий самый глубокий DOM-элемент под this._elem
+   * Приватное свойство _currentTargetElem обновляется при каждом передвижении
+   */
+  getTargetElem() {
+    return this._currentTargetElem;
+  }
+
+  /**
+   * При каждом движении мыши перемещает this._elem
+   * и записывает текущий элемент под this._elem в _currentTargetElem
+   * @param event
+   */
+  onDragMove(event: any) {
+    this._elem.style.left = event.pageX - this._shiftX + 'px';
+    this._elem.style.top = event.pageY - this._shiftY + 'px';
+
+    this._currentTargetElem = getElementUnderClientXY(this._elem, event.clientX, event.clientY);
+  }
+
+  /**
+   * Действия с аватаром, когда перенос не удался
+   * Например, можно вернуть элемент обратно или уничтожить
+   */
+  onDragCancel() {
+    /* override */
+  }
+
+  /**
+   * Действия с аватаром после успешного переноса
+   */
+  onDragEnd () {
+    /* override */
+  }
+}
--- src/TreeDragAvatar.ts
+++ src/TreeDragAvatar.ts
@@ -1,12 +1,12 @@
 declare function extend(Child, Parent) : void
 declare function getCoords(elem) : any
 
-function TreeDragAvatar(dragZone, dragElem) {
-    DragAvatar.apply(this, arguments);
+function TreeDragAvatarOld(dragZone, dragElem) {
+  DragAvatarOld.apply(this, arguments);
 }
-extend(TreeDragAvatar, DragAvatar);
+extend(TreeDragAvatarOld, DragAvatarOld);
 
-TreeDragAvatar.prototype.initFromEvent = function(downX, downY, event) {
+TreeDragAvatarOld.prototype.initFromEvent = function(downX, downY, event) {
     if (event.target.tagName != 'SPAN') return false;
 
     this._dragZoneElem = event.target;
@@ -29,17 +29,59 @@ TreeDragAvatar.prototype.initFromEvent = function(downX, downY, event) {
 /**
  * Вспомогательный метод
  */
-TreeDragAvatar.prototype._destroy = function() {
-    this._elem.parentNode.removeChild(this._elem);
+TreeDragAvatarOld.prototype._destroy = function() {
+  this._elem.parentNode.removeChild(this._elem);
 };
 
 /**
  * При любом исходе переноса элемент-клон больше не нужен
  */
-TreeDragAvatar.prototype.onDragCancel = function() {
+TreeDragAvatarOld.prototype.onDragCancel = function() {
     this._destroy();
 };
 
-TreeDragAvatar.prototype.onDragEnd = function() {
+TreeDragAvatarOld.prototype.onDragEnd = function() {
     this._destroy();
 };
+
+
+class TreeDragAvatar extends DragAvatar{
+  protected initFromEvent(downX: number, downY: number, event: Event) {
+    if (event.target.tagName != 'SPAN') return false;
+
+    this._dragZoneElem = event.target;
+    var elem = this._elem = this._dragZoneElem.cloneNode(true);
+    elem.className = 'avatar';
+
+    // создать вспомогательные свойства shiftX/shiftY
+    var coords = getCoords(this._dragZoneElem);
+    this._shiftX = downX - coords.left;
+    this._shiftY = downY - coords.top;
+
+    // инициировать начало переноса
+    document.body.appendChild(elem);
+    elem.style.zIndex = 9999;
+    elem.style.position = 'absolute';
+
+    return true;
+  }
+
+  /**
+   * Вспомогательный метод
+   */
+  private _destroy () {
+    this._elem.parentNode.removeChild(this._elem);
+  }
+
+  /**
+   * При любом исходе переноса элемент-клон больше не нужен
+   */
+  protected onDragCancel() {
+    this._destroy();
+  }
+
+  protected onDragEnd() {
+    this._destroy();
+  }
+
+}
